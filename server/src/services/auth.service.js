85a627102d33612ca3f84e8a6f2804caa096f3c8
crisppbacon <swankymr@gmail.com>
2026-02-09 14:32:31 +0800

    Delete card.service.js | refractor student and auth services.

--- server/src/services/auth.service.js
+++ server/src/services/auth.service.js
@@ -1,24 +1,68 @@
-/* <--- LOGIN THE USER TO THEIR ACCOUNT ---> */
-export async function login(username, password) {
-  return (username, password);
-}
+import mongoose from 'mongoose';
+import User from '../models/user.model.js';
+import { NotFoundError, UnauthorizedError } from '../utils/errors.js';
 
-/* <--- CREATE AN FOR USER ---> 
-  NOTE: Create a user account for dashboard access.
+/* <--- LOGIN USER ---> 
+    NOTE: LOGIN THE USER TO THEIR ACCOUNT
 */
+export async function login(username, password) {
+  if (!username) throw NotFoundError('Please enter your username');
+  if (!password) throw NotFoundError('Please enter your password');
 
-export async function signup(email, username, password) {
-  return (email, username, password);
+  const user_data = await getUser(username);
+
+  if (!user_data) throw NotFoundError('User not found');
+  if (user_data.password !== password)
+    throw new UnauthorizedError('Incorrect Password');
+
+  delete user_data.password;
+  return user_data;
 }
 
-/* <--- CHECKS IF USER IS LOGGED IN ---> */
-export async function getUserSession(session) {
-  if (!session || !session.user_id) return null;
+/* <--- CREATE USER ---> 
+    NOTE: Create a user account.
+*/
+export async function addUser(user) {
+  const { username, first_name, last_name, email, password } = user;
+  const user_data = new User({
+    first_name,
+    last_name,
+    username,
+    email,
+    password,
+  });
 
-  const user_data = await User.findById(session.user_id)
-    .select('-password -createdAt -updatedAt -__v')
+  await user_data.save();
+  const user_data_object = user_data.toObject();
+  delete user_data_object.password;
+  return user_data_object;
+}
+
+export async function updateUser(user_id) {
+  return user_id;
+}
+
+/* <--- GET USER INFORMATION ---> 
+    NOTE: Fetch User Information From the Database.
+*/
+export async function getUser(identifier) {
+  const query = mongoose.Types.ObjectId.isValid(identifier)
+    ? { _id: identifier }
+    : { username: identifier };
+
+  const user_data = await User.findOne(query)
+    .select('-createdAt -updatedAt -__v')
     .lean();
 
-  if (!user_data) return null;
+  return user_data || null;
+}
+
+/* <--- CHECK LOGIN ---> 
+    NOTE: Checks if session exist and valid, then return user data.
+*/
+export async function getUserBySession(session) {
+  if (!session || !session.user_id) return null;
+  const user_data = getUser(session.user_id);
+  delete user_data.password;
   return user_data;
 }
--- server/src/services/student.service.js
+++ server/src/services/student.service.js
@@ -1,13 +1,44 @@
+import Student from '../models/student.model.js';
+
 /* <--- ADD STUDENT ---> 
     NOTE: Register Student to Database
 */
-export async function addStudent(first_name, last_name, student_id, course) {
-  return (first_name, last_name, student_id, course);
+export async function addStudent(student_data) {
+  const { first_name, last_name, student_id, education } = student_data;
+  const student = new Student({ first_name, last_name, student_id, education });
+  student.save();
+  return student;
 }
 
 /* <--- GET STUDENT INFORMATION ---> 
     NOTE: Fetch Student Information From the Database.
 */
 export async function getStudent(student_id) {
+  const student = await Student.findById(student_id);
+  return student;
+}
+export async function getStudentByCardId(card_id) {
+  const student = await Student.findOne({ card_id }).select(
+    '-guardian -updatedAt -__v'
+  );
+  return student || null;
+}
+export async function getStudents() {
+  const students = await Student.find({}).select('-updatedAt -__v');
+  return students;
+}
+
+/* <--- UPDATE STUDENT INFORMATION ---> 
+    NOTE: Student ID Cards
+*/
+export async function updateStudentCard(student_id, card_id) {
+  const student = await Student.findByIdAndUpdate(student_id, { card_id });
+  return student;
+}
+
+/* <--- DELETE STUDENT INFORMATION ---> 
+    NOTE: Delete Student ID Card Record.
+*/
+export async function deleteStudentCard(student_id) {
   return student_id;
 }
