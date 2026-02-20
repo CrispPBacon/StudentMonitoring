8a543ab8815955d899014d8df10928c6f364c10a
crisppbacon <swankymr@gmail.com>
2026-02-21 00:52:56 +0800

    Add CardID props

--- client/src/features/dashboard/components/forms/AddStudentForm.tsx
+++ client/src/features/dashboard/components/forms/AddStudentForm.tsx
@@ -18,7 +18,8 @@ interface DataProps {
     studentID: string;
     program: string;
     year: string;
-    student_display_photo?: File | undefined
+    student_display_photo?: File | undefined;
+    cardID: string;
 }
 
 
@@ -26,8 +27,11 @@ export default function AddStudentForm() {
     const [firstName, setFirstName] = useState('')
     const [lastName, setLastName] = useState('')
     const [studentID, setStudentID] = useState('')
+    const [cardID, setCardID] = useState('')
     const [program, setProgram] = useState('')
     const [year, setYear] = useState('0')
+    const [guardianPhoneNumber, setGuardianPhoneNumber] = useState('')
+    const [guardianEmail, setGuardianEmail] = useState('')
 
     const [image, setImage] = useState<File | null>(null)
     const fileInputRef = useRef(null);
@@ -49,8 +53,9 @@ export default function AddStudentForm() {
         e.preventDefault();
         // if (!image) return alert("Select a file first");
         const data: DataProps = {
-            firstName, lastName, studentID, program, year
+            firstName, lastName, studentID, program, year, cardID
         }
+
 
         if (image) data['student_display_photo'] = image
         dispatch(addStudent(data))
@@ -64,6 +69,7 @@ export default function AddStudentForm() {
                 <Input value={lastName} onChange={e => setLastName(e.target.value)} placeholder="Last Name" />
             </div>
             <Input value={studentID} onChange={(e) => setStudentID(e.target.value)} placeholder="Student ID" className="mb-4" />
+            <Input value={cardID} onChange={(e) => setCardID(e.target.value)} placeholder="Card ID" className="mb-4" />
             <div className="flex gap-2 mb-4">
                 <select value={program} onChange={(e) => setProgram(e.target.value)} name="course" id="course" className="flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm">
                     <option value="" defaultValue='true' disabled>Select Course</option>
@@ -83,8 +89,8 @@ export default function AddStudentForm() {
             {/* <Input placeholder="Course" type="" className="mb-6" /> */}
 
             <h1 className="text-slate-500 mb-2">Guardian Details</h1>
-            <Input type="text" placeholder="Phone number" className="mb-4" />
-            <Input type="text" placeholder="Email" className="mb-4" />
+            <Input type="text" placeholder="Phone number" className="mb-4" value={guardianPhoneNumber} onChange={e => setGuardianPhoneNumber(e.target.value)} />
+            <Input type="text" placeholder="Email" className="mb-4" value={guardianEmail} onChange={e => setGuardianEmail(e.target.value)} />
 
             <div className="mt-10 gap-2 flex justify-end w-full ">
                 <DialogClose asChild>
