cc6521c8b2fad8f94d8a7d62d4fc959083c8130a
crisppbacon <swankymr@gmail.com>
2026-02-21 00:25:15 +0800

    Update realtime chart feature

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react"
+import { useEffect, } from "react"
 
 
 import { useAppDispatch, useAppSelector } from "@/hooks/reduxHooks";
@@ -6,22 +6,40 @@ import { useAppDispatch, useAppSelector } from "@/hooks/reduxHooks";
 import { Chart, Overview, QuickAccess, StudentTable } from "@/features/dashboard";
 import { fetchStudents } from "@/features/students_list";
 import { fetchAttendanceLog } from "@/features/attendance";
+import type { studentData } from "@/lib/types";
+import { useSocket } from "@/hooks/useSocket";
+import { addAttendanceLog } from "@/features/attendance/attendanceSlice";
+import { getHourlyChartData } from "@/features/dashboard/utils/getHourlyChartData";
 
 
 export default function Dashboard() {
-
     const { students } = useAppSelector((state) => state.student)
-    const { attendanceLog } = useAppSelector((state) => state.attendanceLog)
-
-    const entry = attendanceLog.filter(r => r.type === "entry").length;
-    const exit = attendanceLog.filter(r => r.type === "exit").length;
+    const { exitToday, entryToday, attendanceLog } = useAppSelector((state) => state.attendanceLog)
 
     const dispatch = useAppDispatch()
+    const { socket } = useSocket();
+
 
     useEffect(() => {
         dispatch(fetchStudents())
         dispatch(fetchAttendanceLog())
     }, [dispatch])
+
+    useEffect(() => {
+        const handleAttendance = (data: studentData) => {
+            if (!data) return;
+            const { student_id, createdAt, type } = data
+            const student = students.find(s => s._id === student_id);
+            if (!student) return;
+            const attendanceLog = { student, type, createdAt, }
+            dispatch(addAttendanceLog(attendanceLog))
+        }
+        socket.on("attendance", handleAttendance);
+
+        return () => {
+            socket.off("attendance", handleAttendance);
+        }
+    }, [socket, dispatch, students])
 
 
     return (
@@ -30,13 +48,13 @@ export default function Dashboard() {
                 <h1 className="font-bold text-3xl">Dashboard</h1>
             </div>
             {/* <--- Components ---> */}
-            <Overview totalStudents={students.length} entry={entry} exit={exit} totalTaps={attendanceLog.length} />
+            <Overview totalStudents={students.length} entry={exitToday} exit={entryToday} totalTaps={exitToday + entryToday} />
             <QuickAccess />
 
             <div className=" bg-white p-5 rounded-xl shadow-md">
                 <div className="items-start gap-10 grid grid-cols-1 lg:grid-cols-2">
                     {/* <--- Components ---> */}
-                    <Chart />
+                    <Chart chartData={getHourlyChartData(attendanceLog)} />
                     <StudentTable />
                 </div>
             </div>
