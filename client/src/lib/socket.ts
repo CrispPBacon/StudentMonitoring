1748040def33b50c92135ad8986c15766902359d
crisppbacon <swankymr@gmail.com>
2026-02-21 00:22:44 +0800

    Update socket url to imported env

--- client/src/lib/socket.ts
+++ client/src/lib/socket.ts
@@ -1,5 +1,9 @@
 import { io, Socket } from 'socket.io-client';
+export const backendUrl = `${import.meta.env.VITE_BACKEND_HOST}:${
+  import.meta.env.VITE_BACKEND_PORT
+}`;
 
-export const socket: Socket = io('http://localhost:3000', {
+console.log(backendUrl);
+export const socket: Socket = io(backendUrl, {
   autoConnect: false, // prevents auto-connecting
 });
