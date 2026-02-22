36e55b48368de302b65466fb0505414edf107476
crisppbacon <swankymr@gmail.com>
2026-02-22 18:50:11 +0800

    Update StudentList page realtime

--- client/src/pages/StudentList.tsx
+++ client/src/pages/StudentList.tsx
@@ -1,5 +1,14 @@
+import StudentTable from "@/features/students_list/components/StudentTable";
+
 export default function StudentList() {
     return (
-        <div>StudentList</div>
-    )
+        <main className="p-8 overflow-y-auto rounded-2xl bg-slate-50/95 animate-entry">
+            {/* Header */}
+            <div>
+                <h1 className="text-2xl font-bold text-slate-800">Student Management</h1>
+                <p className="text-sm text-slate-500">List of registered students</p>
+            </div>
+            {/* <--- Components ---> */}
+            <StudentTable />
+        </main >)
 }
