2395c2da4656e8c93f359407d453951f570bf6fa
crisppbacon <swankymr@gmail.com>
2026-02-22 18:52:41 +0800

    Add StudentTable component

--- client/src/features/students_list/index.ts
+++ client/src/features/students_list/index.ts
@@ -1,4 +1,6 @@
 import { addStudent, fetchStudents } from './studentThunks';
 import studentReducer from './studentSlice';
+import StudentTable from './components/StudentTable';
 
 export { addStudent, fetchStudents, studentReducer };
+export { StudentTable };
