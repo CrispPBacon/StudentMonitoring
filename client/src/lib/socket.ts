464919f877b4ba4103cb2aa671466f092ea2eb8b
crisppbacon <swankymr@gmail.com>
2026-02-23 09:54:57 +0800

    Remove console log

--- client/src/lib/socket.ts
+++ client/src/lib/socket.ts
@@ -1,9 +1,6 @@
 import { io, Socket } from 'socket.io-client';
-export const backendUrl = `${import.meta.env.VITE_BACKEND_HOST}:${
-  import.meta.env.VITE_BACKEND_PORT
-}`;
+import { backendUrl } from './api';
 
-console.log(backendUrl);
 export const socket: Socket = io(backendUrl, {
   autoConnect: false, // prevents auto-connecting
 });
