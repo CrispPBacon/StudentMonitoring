1ad3da317383f9dd2f7de2099810460cd3fb1041
crisppbacon <swankymr@gmail.com>
2026-02-24 12:05:24 +0800

    Refactor backend URL construction for improved clarity and flexibility

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -1,9 +1,14 @@
 import axios from 'axios';
 
-export const backendUrl = `${import.meta.env.VITE_BACKEND_HOST}:${
-  import.meta.env.VITE_BACKEND_PORT
-}`;
+const HOST = import.meta.env.VITE_BACKEND_HOST;
+const ip = import.meta.env.VITE_LOCAL_IP;
+const port = import.meta.env.VITE_BACKEND_PORT || 3000;
 
+export const backendUrl = HOST
+  ? `${import.meta.env.VITE_BACKEND_HOST}:${port}`
+  : `http://${ip}:${port}`;
+
+console.log(backendUrl);
 const api = axios.create({
   baseURL: backendUrl,
   withCredentials: true,
