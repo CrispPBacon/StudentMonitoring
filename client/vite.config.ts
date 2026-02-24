9374fddbbd193644815e943b0f2465330eb59f31
crisppbacon <swankymr@gmail.com>
2026-02-24 11:59:24 +0800

    Update define localIP

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,9 +3,17 @@ import react from '@vitejs/plugin-react';
 
 import path from 'path';
 import tailwindcss from '@tailwindcss/vite';
+import { getLocalIPv4 } from './config/getLocalIP';
 
+const localIP = getLocalIPv4();
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: true,
+  },
+  define: {
+    'import.meta.env.VITE_LOCAL_IP': JSON.stringify(localIP),
+  },
   plugins: [react(), tailwindcss()],
   // server: { port: 3000 },
   resolve: {
