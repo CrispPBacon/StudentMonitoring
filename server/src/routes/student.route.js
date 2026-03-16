8e77ed3904063f981f11e1d032d522440113c793
crisppbacon <swankymr@gmail.com>
2026-03-16 20:45:07 +0800

    Update function name convention, remove unused route

--- server/src/routes/student.route.js
+++ server/src/routes/student.route.js
@@ -1,26 +1,22 @@
 import express from 'express';
 import {
-  CreateStudent,
-  DeleteStudent,
-  GetAllStudents,
-  UpdateStudent,
-  UploadDisplayPhoto,
+  createStudent,
+  deleteStudent,
+  getAllStudents,
+  updateStudent,
 } from '../controllers/student.controller.js';
 
 import upload from '../config/multer.js';
 
 const router = express.Router();
 
-router.route('/student/:id').delete(DeleteStudent);
-router.route('/student').get(GetAllStudents);
-router.route('/student').post(upload.single('display_photo'), CreateStudent);
+router.route('/student/:id').delete(deleteStudent);
+router
+  .route('/student')
+  .get(getAllStudents)
+  .post(upload.single('display_photo'), createStudent);
 router
   .route('/student/:id')
-  .post(upload.single('display_photo'), UpdateStudent);
-
-// UPDATE DISPLAYP PHOTO
-router
-  .route('/student/display_photo')
-  .post(upload.single('student_display_photo'), UploadDisplayPhoto);
+  .post(upload.single('display_photo'), updateStudent);
 
 export default router;
