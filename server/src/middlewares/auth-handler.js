1765315368dfd63252d9e9de977c57481cec7bdd
crisppbacon <swankymr@gmail.com>
2026-03-16 20:42:01 +0800

    Update variable and function name convention and add assert middleware

--- server/src/middlewares/auth-handler.js
+++ server/src/middlewares/auth-handler.js
@@ -1,10 +1,14 @@
-import { getUserBySession } from '../services/auth.service.js';
-import { UnauthorizedError } from '../utils/errors.js';
+import { getUser, getUserBySession } from '../services/auth.service.js';
+import {
+  ConflictError,
+  NotFoundError,
+  UnauthorizedError,
+} from '../utils/errors.js';
 
 export async function requireUserSession(req, res, next) {
   try {
-    const user_data = await getUserBySession(req.session);
-    if (!user_data) {
+    const user = await getUserBySession(req.session);
+    if (!user) {
       res.clearCookie('connect.sid');
       throw new UnauthorizedError('Session is invalid or expired');
     }
@@ -12,4 +16,26 @@ export async function requireUserSession(req, res, next) {
   } catch (error) {
     next(error);
   }
+}
+
+export async function assertNoActiveSession(req, res, next) {
+  const user = await getUserBySession(req.session);
+  if (user) return next(new ConflictError('Active session already exists'));
+
+  res.clearCookie('connect.sid');
+  return next();
+}
+
+export async function assertUser(req, _, next) {
+  let query;
+  const { username, email } = req.body || {};
+  if (!username && !email)
+    return next(new NotFoundError('User does not exist'));
+  if (username) query = { username };
+  if (email) query = { email };
+  if (email || username) {
+    const user = await getUser(query);
+    if (!user) next(new NotFoundError('User does not exist'));
+  }
+  return next();
 }
