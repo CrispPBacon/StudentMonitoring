0f2d38eb04514872e25b8cb319a580b9d4a81c58
crisppbacon <swankymr@gmail.com>
2026-03-16 20:40:23 +0800

    Update variable and functions naming convention

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,46 +1,44 @@
-import { addUser, getUserBySession, login } from '../services/auth.service.js';
-import {
-  BadRequestError,
-  NotFoundError,
-  UnauthorizedError,
-} from '../utils/errors.js';
+import { getUserBySession, login, signup } from '#services/auth.service.js';
+import { BadRequestError, UnauthorizedError } from '#utils/errors.js';
 
-export async function AuthLogin(req, res, next) {
+export async function authLogin(req, res, next) {
+  const { email, password } = req.body || {};
   try {
-    const { email, password } = req.body;
+    // NOTE: Validation of fields and user existence
     if (!email || !password)
-      throw new NotFoundError('Please enter your username and password!');
-    const user_data = await login(email, password);
-    req.session.user_id = user_data._id.toString();
-    return res.status(200).json({ data: user_data });
+      throw new BadRequestError('Invalid username or password');
+
+    const user = await login(email, password);
+    req.session.user_id = user._id.toString();
+    return res.status(200).json({ data: user });
   } catch (error) {
     next(error);
   }
 }
 
-export async function AuthSignup(req, res, next) {
+export async function authSignup(req, res, next) {
   try {
-    const user_data = req.body;
-    const data = await addUser(user_data);
+    const user = req.body;
+    const data = await signup(user);
     return res.status(200).json(data);
   } catch (error) {
     next(error);
   }
 }
 
-export async function AuthLogout(req, res, next) {
+export async function authLogout(req, res, next) {
   try {
     if (!req.session || !req.session.user_id)
       throw new BadRequestError('You are not logged in!');
     req.session.destroy();
     res.clearCookie('connect.sid');
     return res.status(200).json({ data: { msg: 'You have logged out' } });
-  } catch (e) {
-    next(e);
+  } catch (error) {
+    next(error);
   }
 }
 
-export async function RefreshAuth(req, res, next) {
+export async function refreshAuth(req, res, next) {
   try {
     const user = await getUserBySession(req.session);
     if (!user) {
@@ -49,7 +47,7 @@ export async function RefreshAuth(req, res, next) {
       throw new UnauthorizedError('Session invalid or does not exist');
     }
     return res.status(200).json({ data: user });
-  } catch (e) {
-    next(e);
+  } catch (error) {
+    next(error);
   }
 }
