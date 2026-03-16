644cf9665509b1d42aa30c6c2f2865e67c9b7fdd
crisppbacon <swankymr@gmail.com>
2026-03-16 21:05:03 +0800

    Minor refractor; Add validation middleware

--- server/src/routes/auth.route.js
+++ server/src/routes/auth.route.js
@@ -1,19 +1,19 @@
 import express from 'express';
 import {
-  AuthLogin,
-  AuthLogout,
-  AuthSignup,
-  RefreshAuth,
-} from '../controllers/auth.controller.js';
-import {
-  validateLogin,
-  validateSignUp,
-} from '../middlewares/validations/validation.js';
+  authLogin,
+  authLogout,
+  authSignup,
+  refreshAuth,
+} from '#controllers/auth.controller.js';
+import { assertNoActiveSession } from '#middlewares/auth-handler.js';
 
 const router = express.Router();
 
 // router.route('/auth').get((req, res) => res.send('LOGIN PAGE'));
-router.route('/auth/login').get(RefreshAuth).post(validateLogin, AuthLogin);
-router.route('/auth/logout').delete(AuthLogout);
-router.route('/auth/signup').post(validateSignUp, AuthSignup);
+router
+  .route('/auth/login')
+  .get(refreshAuth)
+  .post(assertNoActiveSession, authLogin);
+router.route('/auth/logout').delete(authLogout);
+router.route('/auth/signup').post(assertNoActiveSession, authSignup);
 export default router;
