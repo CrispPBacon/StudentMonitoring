03f8d7adb0958d354cd5615eebe4cd887688a56a
crisppbacon <swankymr@gmail.com>
2026-03-16 20:42:36 +0800

    Half measure refractor

--- server/src/controllers/attendance.controller.js
+++ server/src/controllers/attendance.controller.js
@@ -1,44 +1,96 @@
 import {
   getAllAttendanceLog,
   logStudentEntry,
-} from '../services/attendance.service.js';
+} from '#services/attendance.service.js';
 import {
-  getStudentByCardId,
-  getStudentByStudentId,
-} from '../services/student.service.js';
-import { getIO } from '../socket/index.js';
-import { getTodayDateRange } from '../utils/dateRange.js';
+  sendNotificationToParent,
+  sendSMS,
+} from '#services/notification.service.js';
+import { getStudent } from '#services/student.service.js';
+import { getIO } from '#socket/index.js';
+import { getTodayDateRange } from '#utils/dateRange.js';
 
 /* <--- Attendance Controller ---> */
-export async function EntryLog(req, res, next) {
+/**
+ * NOTE: FLOW
+ * *1. GET INPUT FIELDS (card_id, finger_id, student_id)
+ * *2. Replace card_id spaces with underscores
+ * *3. GET Student from database by card or student ID
+ * *4. Return undefined student object and access denied.
+ * *5. Validate fingerprint
+ * *6. 3 seconds delay for card taps
+ * *7. attendanceData object formation
+ * *8. Send SMS and Email
+ * *9. Send response back to client.
+ */
+export async function logExitAndEntry(req, res, next) {
+  const CHECK_FINGERPRINT = true;
+  let { card_id, finger_id } = req.body || {};
+  const { student_id } = req.params || {};
   try {
-    let { card_id, finger_id } = req.body ?? {};
-    if (card_id.includes(' ')) card_id = card_id.replaceAll(' ', '_');
-    if (!card_id) return res.sendStatus(401);
-    // if (finger_id <= 0) return res.sendStatus(401);
+    console.log(student_id);
+    // REVIEW: GET Fields || VALIDATES IF THESE FIELDS EXISTS
+    if ((!card_id && !student_id) || (card_id && !finger_id))
+      return res.sendStatus(401);
+    if (finger_id <= 0 && !student_id && CHECK_FINGERPRINT)
+      return res.sendStatus(401);
+
+    // REVIEW: CARD ID REPLACES SPACES WITH UNDERSCORES (_)
+    if (card_id)
+      if (card_id.includes(' ')) card_id = card_id.replaceAll(' ', '_');
     console.log('CARD ID:', card_id, finger_id);
 
-    const student = await getStudentByCardId(card_id);
-    if (!student) return res.sendStatus(401);
-    // if (!student?.finger_id) return res.sendStatus(401);
-    // if (student.finger_id != finger_id) return res.sendStatus(401);
+    // REVIEW: GET Student by student_id or card_id
+    let student;
+    if (student_id) student = await getStudent({ student_id });
+    if (card_id) student = await getStudent({ card_id });
+
+    // REVIEW: Return attendance Data Object if student not found
+    if (!student_id)
+      if (!student || student?.finger_id != finger_id) {
+        const attendanceData = {
+          _id: undefined,
+          student: {},
+          type: undefined,
+          result: 'denied',
+          createdAt: new Date(),
+        };
+        getIO().emit('attendance', attendanceData);
+
+        return res.sendStatus(401);
+      }
+
+    // REVIEW: Return if finger_id not registered and not equal.
+    if (!student?.finger_id && CHECK_FINGERPRINT) return res.sendStatus(401);
+    if (student.finger_id != finger_id && !student_id && CHECK_FINGERPRINT)
+      return res.sendStatus(401);
 
     // Silent ignore for double tap
     const { log, ignored } = await logStudentEntry(student._id);
-    if (ignored) {
-      return res.sendStatus(429);
-    }
+    if (ignored) return res.sendStatus(429);
 
-    const { display_photo } = student;
-
+    // REVIEW: attendanceData object formation
+    const { display_photo, guardian } = student;
     const { type, createdAt, _id } = log;
     const attendanceData = {
       _id,
-      student: { ...student._doc, display_photo },
+      student: { ...student, display_photo },
       type,
       createdAt,
     };
+    const { email, notification, phone_number } = guardian || {};
+    const { first_name, last_name } = student || {};
+    // console.log({ ...student, display_photo });
 
+    // REVIEW: SEND EMAIL & SMS
+    if (email && email.toUpperCase() != 'N/A' && notification == 'on') {
+      sendNotificationToParent(attendanceData);
+    }
+    if (notification == 'on' && phone_number) {
+      await sendSMS(phone_number, { type, createdAt, first_name, last_name });
+    }
+
+    // REVIEW: Send back response
     getIO().emit('attendance', attendanceData);
     return res.sendStatus(200);
   } catch (error) {
@@ -46,44 +98,11 @@ export async function EntryLog(req, res, next) {
   }
 }
 
-export async function EntryLogByStudentID(req, res, next) {
-  try {
-    const student_id = req.params.student_id;
-
-    if (!student_id) return res.sendStatus(401);
-    const student = await getStudentByStudentId(student_id);
-    if (!student) return res.sendStatus(401);
-
-    const { log, ignored } = await logStudentEntry(student._id);
-    if (ignored) return res.sendStatus(429);
-
-    const { display_photo } = student;
-
-    const { type, createdAt, _id } = log;
-    const attendanceData = {
-      _id,
-      student: { ...student._doc, display_photo },
-      type,
-      createdAt,
-    };
-    // console.log(attendanceData);
-
-    getIO().emit('attendance', attendanceData);
-    return res.status(200).json(attendanceData);
-  } catch (error) {
-    next(error);
-  }
-}
-
-export async function GetAttendanceLog(req, res, next) {
+export async function getAttendanceLog(req, res, next) {
   try {
     let query = {};
-
-    const filter = req.query.filter;
-    const page = req.query.page;
-    const ITEMS_PER_PAGE = req.query.limit || 20;
-
-    // console.log(page, filter);
+    const { filter, page, limit } = req.query || {};
+    const ITEMS_PER_PAGE = limit || 20;
 
     if (filter == 'today') {
       const { startOfDayPH, endOfDayPH } = getTodayDateRange();
