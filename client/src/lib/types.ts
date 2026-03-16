2c7905ce9b8b5490b0a3aeddcb40a53f9eddb69f
crisppbacon <swankymr@gmail.com>
2026-03-16 20:57:25 +0800

    Add props education status, guardian props more details, and remarks

--- client/src/lib/types.ts
+++ client/src/lib/types.ts
@@ -22,12 +22,24 @@ export interface StudentProps {
   first_name: string;
   last_name: string;
   email?: string;
-  education: { category: string; program: string; year: string };
+  education: {
+    category: string;
+    program: string;
+    year: string;
+    status?: 'enrolled' | 'not_enrolled' | 'alumni' | 'dropped';
+  };
   card_id: string;
   student_id: string;
   finger_id?: string;
   display_photo?: File | string;
-  guardian?: { notification: 'on' | 'off' };
+  guardian?: {
+    notification: 'on' | 'off';
+    first_name: string;
+    last_name: string;
+    email: string;
+    phone_number: string;
+  };
+  remarks?: string | undefined;
 }
 
 export interface AttendanceProps {
