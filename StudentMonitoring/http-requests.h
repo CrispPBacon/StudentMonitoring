ff767abd6b37e079ae1aefa2762a28ef31e4457f
crisppbacon <swankymr@gmail.com>
2026-03-16 21:07:13 +0800

    Update IP change to connect to server

--- StudentMonitoring/http-requests.h
+++ StudentMonitoring/http-requests.h
@@ -2,7 +2,7 @@
 #include <HTTPClient.h>
 
 #define PAYLOAD_SIZE 64
-#define SERVER_URL "http://192.168.56.167:3000/api/attendance"
+#define SERVER_URL "http://192.168.209.167:3000/api/attendance"
 
 // char* httpPOST(const char* url, const char* payload);
 bool sendAttendance(const char* cardUID, int fingerID);
